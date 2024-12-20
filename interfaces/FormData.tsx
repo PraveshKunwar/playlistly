606d153cf72696c7e82b5c2686c7ca65cb0a7985
PraveshKunwar <praveshk04@outlook.com>
2024-12-20 18:24:23 -0500

    fixing: gitignore pls

--- interfaces/FormData.tsx
+++
@@ -1,8 +0,0 @@
-export type time = "long_term" | "medium_term" | "short_term";
-export type type = "artists" | "tracks";
-
-export interface FormData {
-  time_range: time | undefined;
-  typeOf: type | undefined;
-  limit: number | undefined;
-}
